76032d6d7a6e743ace136f5e6dd3af8d9f10dbd8
Danny4life <osiki.segun@gmail.com>
2023-10-21 21:22:30 +0100

    add login and create an account menu items to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="container flex justify-center items-center">
+    <div className="container flex justify-center items-center pt-8">
         <div className="flex-1 justify-center items-center flex">
             <h1 
                 className="w-56 h-10 font-bold text-2xl leading-9 items-center text-[#012A4A]"
@@ -26,7 +26,16 @@ const Navbar = () => {
                 </li>
             </ul>
         </div>
-        <div className="flex-1">3</div>
+        <div className="flex-1 font-bold text-base leading-5">
+            <ul className="flex justify-end space-x-10 items-center cursor-pointer hover:cursor-pointer">
+                <li>
+                    <a className="w-11 h-5">Login</a>
+                </li>
+                <li>
+                    <a className="w-48 h-12 rounded-md py-4 px-4 bg-blue-700 text-base text-white text-center">Create an account</a>
+                </li>
+            </ul>
+        </div>
     </div>
   )
 }
