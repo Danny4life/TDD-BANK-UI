61b097e12bdb6846e7442235a4408f5e3fae863e
Danny4life <osiki.segun@gmail.com>
2023-10-24 09:47:39 +0100

    create section two component

---
+++ src/components/SectionTwo.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const SectionTwo = () => {
+    return (
+        <section className="w-[1440px] h-[742px] bg-[#D8F3DC] flex">
+        <div className="flex-1 justify-center ml-10 pt-48 flex">
+            <div className="">
+                <h1 className="w-[605px] h-[183px] font-bold text-5xl leading-[60.51px] text-[#3A0CA3]">
+                Quick and easy payment platform for all your transactions</h1>
+                <p className="w-[498px] h-[48px] font-normal text-base leading-6 text-[#012A4A] pt-4">Save and manage all your transaction in one place, easy payment 
+                    anytime & anyday.
+                </p>
+            </div>
+           
+        </div>
+        <div className="flex-1 pt-24 flex justify-center">
+            <div className="">
+                
+            </div>
+        </div>
+    
+        </section>
+      )
+}
+
+export default SectionTwo
