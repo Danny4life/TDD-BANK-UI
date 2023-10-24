7edae9970b83498557edca6bf20899d17f05d128
Danny4life <osiki.segun@gmail.com>
2023-10-24 09:51:10 +0100

    add title and paragraph to first section

--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 const SectionTwo = () => {
     return (
-        <section className="w-[1440px] h-[742px] bg-[#D8F3DC] flex">
+        <section className="w-[1440px] h-[742px] bg-[white] flex">
         <div className="flex-1 justify-center ml-10 pt-48 flex">
             <div className="">
                 <h1 className="w-[605px] h-[183px] font-bold text-5xl leading-[60.51px] text-[#3A0CA3]">
-                Quick and easy payment platform for all your transactions</h1>
-                <p className="w-[498px] h-[48px] font-normal text-base leading-6 text-[#012A4A] pt-4">Save and manage all your transaction in one place, easy payment 
-                    anytime & anyday.
+                Get the convenience of transacting with our services</h1>
+                <p className="w-[498px] h-[48px] font-normal text-base leading-6 text-[#012A4A] pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat eget ipsum, sed praesent.
                 </p>
             </div>
            
