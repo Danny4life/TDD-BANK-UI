083b9add156386c047a8f8a363d92e47f19f5e5a
Danny4life <osiki.segun@gmail.com>
2023-10-27 11:02:58 +0100

    edit section three component

--- src/components/SectionThree.jsx
+++ src/components/SectionThree.jsx
@@ -3,8 +3,8 @@ import imgFive from "../assets/imgFive.png";
 
 const SectionThree = () => {
   return (
-    <section className="w-full h-[620px] bg-slate-200 flex">
-    <div className="flex-1 justify-center ml-8 pt-48 flex">
+    <section className="w-full h-[453px] bg-slate-200 flex">
+    <div className="flex-1 justify-center items-center ml-8 flex">
         <div className="">
             <div className="pt-12">
                 <img src={imgFive} className="" />
@@ -13,12 +13,12 @@ const SectionThree = () => {
         </div>
        
     </div>
-    <div className="flex-1 pt-56 flex justify-center">
+    <div className="flex-1 flex justify-center items-center">
         <div className="">
-            <h1 className="w-[433px] h-[96px] font-bold text-5xl leading-[60.51px] text-[#3A0CA3]">
+            <h1 className="w-[433px] h-[96px] font-bold text-[40px] leading-[60.51px] text-[#3A0CA3]">
                 Choose how you want to make transfers
             </h1>
-            <p className="w-[498px] h-[48px] font-normal text-base leading-6 text-[#012A4A] pt-24">
+            <p className="w-[416px] h-[48px] font-normal text-base leading-6 text-[#012A4A] pt-8">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat
                 eget ipsum, sed praesent.
             </p>             
